00c2675598321fa7599be6690ee45b3a6b786a4f
Gianluca Mariano <giangimar04@gmail.com>
2023-12-05 21:38:38 +0100

    Ver 1.0

--- main.js
+++ main.js
@@ -70,19 +70,17 @@ function salvaContatto() {
     
     <div id="contatto-${indexrubrica}" style="height: 10%; width: 100%; background-color: black; border-bottom: 2px solid gray; color: white; display: flex; justify-content: space-between; align-items: center;">
     
-    <h3 id="nome" style="margin-left: 50px;">${nome}</h3>
+    <h3 id="nome-" style="margin-left: 50px;">${nome}</h3>
 
-    <h3 id="cognome" style="margin-left: -370px;">${cognome}</h3>
+    <h3 id="cognome-" style="margin-left: -370px;">${cognome}</h3>
     
-    <h3 id="numero">${numero}</h3>
+    <h3 id="numero-">${numero}</h3>
     
     <button onclick="modificaContatto(${indexrubrica})"  style="border: 2px solid white; border-radius: 100px; color: white; background-color: black;">Edit</button>
     
     <button onclick="eliminaContatto(${indexrubrica})" style="margin-right: 30px; margin-right: 30px; border: 2px solid red; border-radius: 100px; color: red; background-color: black;" >X</button>
     
     </div>
-    
-    <!-- V Modifica contatto qui V -->
     
     <div id="contatto-edit-${indexrubrica}" style="height: 10%; width: 100%; background-color: black; border-bottom: 2px solid gray; color: white; display: none; justify-content: space-between; align-items: center;">
     
@@ -138,6 +136,8 @@ function annullaModificaContatto(index) {
 
     document.getElementById("contatto-edit-"+index).style.display = "none"
 
+
+
     document.getElementById("contatto-"+index).style.display = "flex"
 }
 
@@ -151,6 +151,7 @@ function confermaModificaContatto(index) {
 
         
         document.getElementById("nome_edit_"+index).style.borderColor = "red";
+        return
 
 
 
@@ -158,18 +159,59 @@ function confermaModificaContatto(index) {
 
         
         document.getElementById("cognome_edit_"+index).style.borderColor = "red";
-
+        return
 
         
     }  if (!nuovo_numero) {
 
         
         document.getElementById("numero_edit_"+index).style.borderColor = "red";
-
+        return
         
     }
 
+    document.getElementById("contatto-"+index).innerHTML = `
     
+    <h3 id="nome-" style="margin-left: 50px;">${nuovo_nome}</h3>
+
+    <h3 id="cognome-" style="margin-left: -370px;">${nuovo_cognome}</h3>
+    
+    <h3 id="numero-">${nuovo_numero}</h3>
+    
+    <button onclick="modificaContatto(${index})"  style="border: 2px solid white; border-radius: 100px; color: white; background-color: black;">Edit</button>
+    
+    <button onclick="eliminaContatto(${index})" style="margin-right: 30px; margin-right: 30px; border: 2px solid red; border-radius: 100px; color: red; background-color: black;" >X</button>`
+
+    document.getElementById("contatto-edit-"+index).innerHTML = `
+
+    <input id="nome_edit_${index}" type="text" style="margin-left: 30px; border-radius: 20px; border: 2px solid gray; height: 40px; color: gray; background-color: black; color: white;" placeholder="${nuovo_nome}">
+
+    <input id="cognome_edit_${index}" type="text" style="margin-left: -250px;; border-radius: 20px; border: 2px solid gray; height: 40px; color: gray; background-color: black; color: white;" placeholder="${nuovo_cognome}">
+    
+    <input id="numero_edit_${index}" style="border-radius: 20px; border: 2px solid gray; height: 40px; color: gray; background-color: black; color: white;" type="text" placeholder="${nuovo_numero}">
+    
+    <button onclick="confermaModificaContatto(${index})" id="salva" style="border: 2px solid rgb(1, 251, 1); border-radius: 100px; color: rgb(1, 251, 1); background-color: black;" >Salva</button>
+    
+    <button onclick="annullaModificaContatto(${index})" id="annulla" style="margin-right: 30px; border: 2px solid red; border-radius: 100px; color: red; background-color: black;" >Annulla</button>
+    `
+
+    rubrica[index].nome = nuovo_nome;
+    rubrica[index].cognome = nuovo_cognome;
+    rubrica[index].numero = nuovo_numero;
+
+    
+
+    document.getElementById("contatto-edit-" + index).style.display = "none";
+    document.getElementById("contatto-" + index).style.display = "flex";
+
+
+}
+
+function eliminaContatto(index) {
+
+    document.getElementById("contatto-"+index).remove();
+    document.getElementById("contatto-edit-"+index).remove();
+
 
 
 }
